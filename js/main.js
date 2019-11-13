bf896764ae9c77f10bca211889bc939ee0576b84
QinXuYang <1446445040@qq.comm>
2019-11-13 19:26:37 +0800

    基本功能完成，可以正常答题，但结构还需要优化

--- js/main.js
+++ js/main.js
@@ -1,9 +1,122 @@
-var question = new Question("file")
-var view = new QuestionView("question", "list")
-console.log(view)
+/**
+ * @param {Object} id          上传文件id
+ * @param {Object} callback    回调函数
+ */
+var loadQuestion = function(id, callback){
+	let input = document.getElementById(id)
+	input.addEventListener("change", function(event){
+		let files = event.target.files,
+			fr = new FileReader()
+		fr.onload = function(e){
+			try{
+				var data = e.target.result,
+					workbook = XLSX.read(data, {
+						type: 'binary'
+					}),
+					// 存储数据
+					result = []
+			}catch(e){
+				console.error("文件类型不正确")
+				return
+			}
+			// 遍历每张表
+			for (let sheet in workbook.Sheets) {
+				if (workbook.Sheets.hasOwnProperty(sheet)){
+					let datas = workbook.Sheets[sheet]
+					result = result.concat(XLSX.utils.sheet_to_json(datas))
+					break //读取第一张表，选择题
+				}
+			}
+			
+			// 格式化题库
+			let questions = [],
+				//缓存push方法
+				push = [].push
+			for (let question of result){
+				// 每一道题目的缓存
+				let temp = {}
+				Object.keys(question).forEach(function(item){
+					push.call(temp, question[item])
+				})
+				questions.push(temp)
+			}
+			
+			// 回调函数传入数据
+			callback(questions)
+		}
+		fr.onprogress = function(event){
+			console.log(event.loaded/files[0].size*100 + '%')
+		}
+		fr.readAsBinaryString(files[0])
+	})
+}
+/**
+ * 作为回调函数,该部分是逻辑控制的核心
+ * @param {Object} result   文件读取成功后的数据
+ */
+var main = function(result){
+	// 创建题目数据管理对象
+	var question = new Question(result),
+		// 视图管理对象
+		view = new QuestionView("container")
 
-var iter = question.iterator()
-view.showQuestion({
-	question: "nihao",
-	list: [1,2,3,4]
-})
+	//创建题目迭代器
+	var iter = question.iterator()
+
+	// 第一次渲染题目
+	view.showQuestion(iter.first())
+
+	/**
+	 * 前后题目切换模块,需要解决数据耦合,所以在外部实现
+	 * @param  {[type]} view [视图对象]
+	 * @param  {[type]} iter [数据迭代器]
+	 */
+	var preHandler = function(view, iter){
+		let data = iter.pre()
+		if(data){
+			view.showQuestion(data)
+		}
+	},
+	nextHandler = function(view, iter){
+		let data = iter.next()
+		if(data){
+			view.showQuestion(data)
+		}
+	}
+	// 函数柯里化传入额外数据重写函数
+	preHandler = preHandler.bind(view.preBtn, view, iter)
+	nextHandler = nextHandler.bind(view.nextBtn, view, iter)
+	// 绑定事件
+	view.preBtn.addEventListener("click", preHandler, false)
+	view.nextBtn.addEventListener("click", nextHandler, false)
+
+
+	// 创建题目状态管理模块,参数为id
+	question.createManager("stateManager")
+	// 点击切换题目
+	stateHandler = stateHandler.bind(question, view, iter)
+	question.stateManager.addEventListener("click", stateHandler, false)
+	function stateHandler(view, iter, event){
+		var target = event.target,
+			// 序号
+			num = parseInt(target.innerHTML)
+		// 加载对应题号的题目
+		view.showQuestion(iter.get(num-1))
+
+		// 设置背景颜色
+		Observer.fire("changeStateColor",{
+			color: "active",
+			// 当前元素和上一个元素
+			target: target,
+			preEle: this.preEle
+		})
+		// 将点击过的元素缓存起来以便后面使用
+		this.preEle = target
+	}
+
+
+	Observer.setWatcher("question",question)
+	Observer.setWatcher("view",view)
+}
+//加载题目并执行回调函数
+loadQuestion("file", main)
--- js/question.js
+++ js/question.js
@@ -1,52 +1,53 @@
-loadQuestion: function(id){
-	let input = document.getElementById(id),
-		// 保存this
-		_this = this
-	input.addEventListener("change", function(event){
-		let files = event.target.files,
-			fr = new FileReader()
-		fr.onload = function(e){
-			try{
-				var data = e.target.result,
-					workbook = XLSX.read(data, {
-						type: 'binary'
-					}),
-					// 存储数据
-					result = []
-			}catch(e){
-				console.error("文件类型不正确")
-				return
-			}
-			// 遍历每张表
-			for (let sheet in workbook.Sheets) {
-				if (workbook.Sheets.hasOwnProperty(sheet)){
-					let datas = workbook.Sheets[sheet]
-					result = result.concat(XLSX.utils.sheet_to_json(datas))
-				}
-			}
-			// 保存题库
-			this.questions = result
-			console.log("读取成功")
-		}
-		fr.readAsBinaryString(files[0])
-	})
-}
-
 /**
  * 题目数据管理模块
  * @param {Object} id  上传文件的元素id
  */
-var Question = function(){
-	// 题库数据
-	this.questions = []
+var Question = function(data){
 	// 加载题库
-	this.loadQuestion(id)
+	this.questions = data
+	this.stateManager = null
+	// 上一个元素的缓存
+	this.preEle = null
+	// 将题库数据根据视图进行适配
+	this.init(data)
 }
 Question.prototype = {
+	// 适配数据
+	init: function(data){
+		// 缓存适配数据
+		let result = []
+		data.forEach(function(item){
+			// 构造适合视图的数据
+			let temp = {
+				num: parseInt(item[0]),
+				question: item[1].trim(),
+				list: [item[2], item[3], item[4], item[5]],
+				answer: item[6].trim()
+			}
+			result.push(temp)
+		})
+
+		if (isNaN(result[0].num)) {
+			result.shift()
+		}
+		// 替换原来的数据
+		this.questions = result
+		console.log(this.questions)
+	},
 	/**
-	 * 读取excel文件数据
+	 * 题目状态管理,对错,以及题目数量等等
 	 */
-	
+	createManager: function(id){
+		var div = document.getElementById(id),
+			// 视图缓存
+			html = ''
+		// i从1开始,因为第一行是表头
+		for (let i=1; i<this.questions.length; i++) {
+			html += `<div>${i}</div>`
+		}
+		div.innerHTML = html
+		this.stateManager = div
+	},
 	/**
 	 * 题目迭代器
 	 */
@@ -55,10 +56,9 @@ Question.prototype = {
 			throw new Error("数据还未读取！")
 		// 当前索引值，默认为0
 		var index = 0,
-			items = this.questions;
+			items = this.questions
 		return {
 			first: function(){
-				console.log(this.questions)
 				index = 0  // 矫正当前索引
 				return items[index]
 			},
@@ -67,7 +67,8 @@ Question.prototype = {
 				return items[index]
 			},
 			pre: function(){
-				if(--index > 0){  //  如果索引值大于零获取相应元素
+				console.log(index)
+				if(--index >= 0){  //  如果索引值大于等于零获取相应元素
 					return items[index]
 				} else {
 					index = 0
@@ -78,7 +79,8 @@ Question.prototype = {
 				if(++index < items.length){  // 如果索引在范围内就获取元素
 					return items[index]
 				} else {
-					index = length - 1
+					//矫正index
+					index = items.length - 1
 					return null
 				}
 			},
@@ -86,7 +88,7 @@ Question.prototype = {
 				if (num >= 0 && num <items.length) {
 					index = num  // 矫正index
 					return items[index]
-				}
+				} else return null
 			}
 		}
 	}
@@ -94,22 +96,141 @@ Question.prototype = {
 
 /**
  * 题目视图管理模块
- * @param {Object} quesTitle  题目元素id
- * @param {Object} quesItem   选项列表元素id
+ * @param {Object} id  将视图渲染到对应id的dom元素中
  */
-var QuestionView = function(quesTitleID, quesListID){
-	this.question = document.getElementById(quesTitleID)
-	this.quesList = document.getElementById(quesListID)
+var QuestionView = function(id){
+	this.init(id)
+	// 当前题目信息
+	this.currentQuestion = null
 }
 QuestionView.prototype = {
+	/**
+	 * 初始化视图
+	 */
+	init: function(id){
+		let html = `
+			<div class="panel panel-default questionBox">
+				<div class="panel-heading header">
+					<p id="title"></p>
+				</div>
+				<div class="panel-body body">
+					<ol type="A" id="list">
+						<li answer="A"></li>
+						<li answer="B"></li>
+						<li answer="C"></li>
+						<li answer="D"></li>
+					</ol>
+				</div>
+				<div class="panel-footer footer">
+					<button type="button" id="preBtn" class="btn btn-default">上一题</button>
+					<button type="button" id="nextBtn" class="btn btn-default">下一题</button>
+				</div>
+			</div>
+		`
+		// 初始化试图
+		let div = document.getElementById(id)
+		div.innerHTML = html
+		// 初始化视图组件
+		this.question = document.getElementById("title") //题目
+		this.quesList = document.getElementById("list")  //选项列表
+		this.preBtn = document.getElementById("preBtn")  //上一题按钮
+		this.nextBtn = document.getElementById("nextBtn")//下一题按钮
+	},
 	/**
 	 * 渲染数据
 	 * @param {Object} data
 	 */
 	showQuestion: function(data){
-		this.question.innerText = data.question
-		Array.prototype.forEach.call(this.quesList.children, function(item, index){
+		// 设置题目
+		this.question.innerText = data.num + ". " + data.question
+		// 遍历,设置选项
+		let list = this.quesList.children
+		Array.prototype.forEach.call(list, function(item, index){
 			item.innerText = data.list[index]
 		})
+		//设置当前题目
+		this.currentQuestion = data
+		// 重置选项颜色
+		this.resetColor()
+		//设置正确答案
+		this.answer = data.answer
+		//重新绑定事件
+		this.bindEvent()
+	},
+	/**
+	 * 事件委托,绑定验证逻辑的事件
+	 */
+	bindEvent: function(){
+		let _this = this
+		// 移除之前添加但可能未被触发的事件,防止重复触发
+		_this.quesList.onclick = null
+		// 使用dom0级方式绑定事件,便于清除事件
+		_this.quesList.onclick = function(event){
+			let target = event.target
+			// 由于利用委托,要防止点到父元素
+			if (target.nodeName != "LI") {
+				return
+			}
+
+			// flag为返回的判断结果对象
+			let judgeResult = _this.judge(target)
+			// 如果答题正确正确,自动下一题,并更新state状态
+			if(judgeResult.status){
+				setTimeout(function(){
+					_this.nextBtn.click()
+				}, 200)
+			}
+			judgeResult.quesNum = _this.currentQuestion["num"]
+			Observer.fire("viewToState", judgeResult)
+			
+			// 触发之后移除事件防止多次触发
+			_this.quesList.onclick = null
+		}
+	},
+	/**
+	 * 验证答案并设置题目状态
+	 * @param  {[type]} myItem    [我的答案]
+	 */
+	judge: function(myItem){
+		//获取选项列表
+		let list = this.quesList.children,
+			//获取我的答案
+			myAns = myItem.getAttribute("answer"),
+			//获取正确答案
+			rightAns = this.currentQuestion["answer"],
+			// 题正误状态
+			status = true,
+			// 获取正确项
+			rightItem
+		// 通过正确答案找到正确项
+		for (let item of list) {
+			let ans = item.getAttribute("answer")
+			if (ans == rightAns) {
+				rightItem = item
+			}
+		}
+
+		// 比对正确项与我的选项,并设置状态
+		if (myItem == rightItem) {
+			myItem.classList.add("success")
+		} else {
+			myItem.classList.add("failed")
+			rightItem.classList.add("success")
+			status = false
+		}
+		return {
+			status: status,
+			answer: myAns,
+			rightAns: rightAns
+		}
+	},
+	/**
+	 * [切换题目时,重置颜色]
+	 */
+	resetColor: function(){
+		let list = this.quesList.children
+		for (let item of list) {
+			item.className = ""
+		}
 	}
 }
