1de09d1fd7b72a8499dd0786584e9253fd53b6df
QinXuYang <1446445040@qq.comm>
2019-11-22 18:22:14 +0800

    开始重新布局

--- 前端/js/main.js
+++ 前端/js/main.js
@@ -5,7 +5,7 @@ const FILES = "http://47.102.206.14:3000/files"
 const QUESTION = "http://47.102.206.14:3000/question"
 //本地接口
 // const FILES = "http://localhost:3000/files"
-// const QUESTION = "http://47.102.206.14:3000/question"
+// const QUESTION = "http://localhost:3000/question"
 var storage = window.sessionStorage
 /**
  * 事件防抖函数
@@ -63,22 +63,15 @@ $(function(){
 	var files = storage.getItem("quesList")
 	files = JSON.parse(files)
 	if (!files) {
-		let xhr = new XMLHttpRequest()
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState == 4 && xhr.status == 200){
-				// 先转化为对象
-				files = JSON.parse(xhr.responseText)
-				// 再重新定义数据
-				files = files.map(function(item){
-					// 去除文件后缀名
-					return item.slice(0,item.indexOf("."))
-				})
-				storage.setItem("quesList", JSON.stringify(files))
-				setList(files)  // 渲染列表
-			}
-		}
-		xhr.open('get', FILES)
-		xhr.send(null)
+		$.get(FILES, function(data, status){
+			// 重新定义数据
+			files = data.map(function(item){
+				// 去除文件后缀名
+				return item.slice(0,item.indexOf("."))
+			})
+			storage.setItem("quesList", JSON.stringify(files))
+			setList(files)  // 渲染列表
+		})
 	} else {
 		setList(files)  // 渲染题目列表
 	}
@@ -90,7 +83,7 @@ var ulClickHandler = function(event){
 		file = target.innerText
 	// 过滤事件
 	if(target.parentNode != ul){
-		return
+		return false
 	}
 	// 检查本地有没有缓存当前题目信息
 	var	data = storage.getItem(file)
@@ -98,32 +91,24 @@ var ulClickHandler = function(event){
 		main(JSON.parse(data))
 	} else {
 		// 如果本地不存在则从服务器获取数据
-		var xhr = new XMLHttpRequest()
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState == 4 && xhr.status == 200){
-				let result = JSON.parse(xhr.responseText)
-				if (result.success) {
-					main(result.data)
-					// 将数据缓存到本地,以便下次访问
-					storage.setItem(file, JSON.stringify(result.data))
-				} else {
-					main(null)
-				}
+		$.post(QUESTION, {filename: file}, function(data, status){
+			let result = data
+			if (result.success) {
+				main(result.data)
+				// 将数据缓存到本地,以便下次访问
+				storage.setItem(file, JSON.stringify(result.data))
+			} else {
+				main(null)
 			}
-		}
-		xhr.open('post', QUESTION)
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.send(`filename=${file}`)
+		})
 	}
-}
-// 题库列表绑定事件,事件委托
-ul.addEventListener("click", debounce(ulClickHandler, 150), false)
-ul.addEventListener("click", function(event){
 	$(event.target).addClass("active").siblings().removeClass("active")
 	// 关闭题库面板
 	$(ul).slideToggle(200)
 	$("#answerSheet").css("visibility","visible")
-}, false)
+}
+// 题库列表绑定事件,事件委托
+ul.addEventListener("click", debounce(ulClickHandler, 150), false)
 var btnClickHandler = function(event){
 	$(ul).slideToggle(200)
 }
